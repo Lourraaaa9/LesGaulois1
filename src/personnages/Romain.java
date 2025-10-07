652976a51bdf7b43705e0d138ef068f49fca04bf
Lourraaaa9 <l.culenaire@gmail.com>
2025-10-07 09:46:18 +0200

    tp2 fin

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,16 +3,22 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	
+	private boolean isInvariantVerified() {
+		return force>=0;
+	}
+	
 	public Romain(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 	
+	@Override
 	public String toString() {
 		return nom;
 	}
@@ -27,6 +33,8 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup>0;
+		int forceAvant=force;
 		this.force=this.force - forceCoup;
 		if (this.force<=0) {
 			this.parler("J'abandonne !");
@@ -34,6 +42,11 @@ public class Romain {
 		else {
 			this.parler("Aie");
 		}
+		assert forceAvant > force;
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
 	}
 
 }
